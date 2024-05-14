e63e5eecf60cf07b2eae3257e67cd7816a6484f6
Dmitry Krizo <staizyy@yandex.ru>
2024-05-14 19:14:58 +0700

    dodelat' delete

---
+++ Controllers/CarController.cs
@@ -0,0 +1,149 @@
+﻿using DriveForum.DatabaseContext;
+using DriveForum.Models;
+using DriveForum.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace DriveForum.Controllers
+{
+    public class CarController : Controller
+    {
+        ApplicationContext _context;
+
+        public CarController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("moderator")]
+        public async Task<IActionResult> NewCar()
+        {
+            CarsForModerator carsForModerator = new()
+            {
+                Cars = await _context.Cars.Include(u => u.Model.Brand.CarModels).Include(u => u.Engine).ToListAsync(),
+                Brands = await _context.CarBrands.Include(u => u.CarModels).ToListAsync(),
+                Engines = await _context.CarEngines.ToListAsync()
+            };
+            //List<Car> cars = await _context.Cars.Include(u => u.Model.Brand.CarModels).Include(u => u.Engine).ToListAsync();
+            return View(carsForModerator);
+        }
+
+        /*[HttpPost]
+        public async Task<IActionResult> NewCar(int carbrandid, int carmodelid, int engineid)
+        {
+            var newcarbrand = _context.CarBrands.FindAsync(carbrandid);
+            var newcarmodel = _context.CarModels.FindAsync(carmodelid);
+            var newengine = _context.CarEngines.FindAsync(engineid);
+            CarModel newcarmodel = new() { }
+            Car newcar = new() { Engine = newengine, Model = }
+            _context.Cars.Add()
+
+            return View();
+        }*/
+
+        /*public async Task<IActionResult> NewBrand(string brandname, string country)
+        {
+            if (_context.CarBrands.Where(u=>u.Name == brandname).FirstOrDefault() == null)
+            {
+                CarBrand newcarbrand = new CarBrand { Name = brandname, Country = country, CarModels = new() };
+                _context.CarBrands.Add(newcarbrand);
+                await _context.SaveChangesAsync();
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> NewModel(string brandname, string country)
+        {
+            if (_context.CarBrands.Where(u => u.Name == brandname).FirstOrDefault() == null)
+            {
+                CarBrand newcarbrand = new CarBrand { Name = brandname, Country = country, CarModels = new() };
+                _context.CarBrands.Add(newcarbrand);
+                await _context.SaveChangesAsync();
+            }
+            return View();
+        }*/
+        [HttpPost]
+        [Route("moderator/add")]
+        public async Task<IActionResult> NewCar(string brandname, string country, string modelname, string modelyear, string enginename, string enginecapacity)
+        {
+            if (brandname != null && country != null && modelname == null && modelyear == null && enginename == null && enginecapacity == null)
+            {
+                if ((_context.CarBrands.FirstOrDefault(b => b.Name == brandname && b.Country == country)) == null)
+                {
+                    CarBrand newCarBrand = new CarBrand() { Name = brandname, Country = country, CarModels = new() };
+                    _context.CarBrands.Add(newCarBrand);
+                    _context.SaveChanges();
+                }
+            }
+            else if (brandname == null && country == null && modelname == null && modelyear == null && enginename != null && enginecapacity != null)
+            {
+                float enginecapacityfloat = float.Parse(enginecapacity.Replace('.', ','));
+                if ((_context.CarEngines.FirstOrDefault(e => e.Name == enginename && e.Capacity == enginecapacityfloat) == null))
+                {
+                    CarEngine newCarEngine = new CarEngine() { Name = enginename, Capacity = enginecapacityfloat };
+                    _context.CarEngines.Add(newCarEngine);
+                    _context.SaveChanges();
+                }
+            }
+
+            else if (brandname != null && country != null && modelname != null && modelyear != null && enginename == null && enginecapacity == null)
+            {
+                int modelyearint = int.Parse(modelyear);
+                var existingBrand = _context.CarBrands.Include(u=>u.CarModels).FirstOrDefault(b => b.Name == brandname && b.Country == country);
+                if (existingBrand == null)
+                {
+                    CarBrand newCarBrand = new CarBrand() { Name = brandname, Country = country, CarModels = new() };
+                    existingBrand = newCarBrand;
+                    _context.CarBrands.Add(newCarBrand);
+                }
+                var existingModel = existingBrand?.CarModels?.FirstOrDefault(m => m.Name == modelname && m.Year == modelyearint);
+                if (existingModel == null)
+                {
+                    CarModel newCarModel = new CarModel() { Name = modelname, Year = modelyearint, Brand = existingBrand };
+                    _context.CarModels.Add(newCarModel);
+                }
+                _context.SaveChanges();
+            }
+
+            else if (brandname != null && country != null && modelname != null && modelyear != null && enginename != null && enginecapacity != null)
+            {
+                float enginecapacityfloat = float.Parse(enginecapacity.Replace('.', ','));
+                int modelyearint = int.Parse(modelyear);
+                CarBrand existingBrand = _context.CarBrands.Include(u=>u.CarModels).FirstOrDefault(b => b.Name == brandname && b.Country == country);
+                CarModel existingModel = _context.CarModels.FirstOrDefault(m => m.Name == modelname && m.Year == modelyearint && m.Brand == existingBrand);
+                CarEngine existingEngine = _context.CarEngines.FirstOrDefault(e => e.Name == enginename && e.Capacity == enginecapacityfloat);
+
+                if (existingBrand != null && existingModel != null && existingEngine != null)
+                {
+                    Car newCar = new Car() { Model = existingModel, Engine = existingEngine };
+                    _context.Cars.Add(newCar);
+                    _context.SaveChanges();
+                }
+            }
+            else
+            {
+                return Redirect("../moderator");
+            }
+            return Redirect("../moderator");
+        }
+        [Route("/delete")]
+        public async Task<IActionResult> Delete(int? carid, int engineid)
+        {
+            Car? car = await _context.Cars.FindAsync(carid);
+            if (car != null)
+            {
+                _context.Cars.Remove(car);
+                _context.SaveChanges();
+            }
+            CarEngine? engine = await _context.CarEngines.FindAsync(engineid);
+            if (engine != null)
+            {
+                _context.CarEngines.Remove(engine);
+                _context.SaveChanges();
+            }
+            return Redirect("../moderator");
+        }
+    }
+}
---
+++ ViewModels/CarsForModerator.cs
@@ -0,0 +1,11 @@
+﻿using DriveForum.Models;
+
+namespace DriveForum.ViewModels
+{
+    public class CarsForModerator
+    {
+        public List<Car> Cars { get; set; }
+        public List<CarBrand> Brands { get; set; }
+        public List<CarEngine> Engines { get; set; }
+    }
+}
